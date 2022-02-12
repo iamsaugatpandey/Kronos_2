34f8112fe2f9aa5b69172b180770587dde2a36e4
barattal <98560246+barattal@users.noreply.github.com>
2022-02-11 20:38:23 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ let width = 800, height = 700;
 
 var projection = d3.geo.mercator()
     .scale(1500)
-    // Center the Map in Colombia
-    .center([-74, 4.5])
     .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
@@ -17,25 +15,13 @@ var svg = d3.select('svg')
 
 var g = svg.append('g');
 
-var effectLayer = g.append('g')
-    .classed('effect-layer', true);
-
 var mapLayer = g.append('g')
     .classed('map-layer', true);
 
-var dummyText = g.append('text')
-    .classed('dummy-text', true)
-    .attr('x', 10)
-    .attr('y', 30)
-    .style('opacity', 0);
-
-var bigText = g.append('text')
-    .classed('big-text', true)
-    .attr('x', 20)
-    .attr('y', 45);
 d3.json('/data/Abila.geojson', function (error, mapData) {
     var features = mapData.features;
-
+    projection.fitSize([width,height],mapData);
+    
     // Update color scale domain based on data
 
     // Draw each province as a path
@@ -43,7 +29,6 @@ d3.json('/data/Abila.geojson', function (error, mapData) {
         .data(features)
         .enter().append('path')
         .attr('d', path)
-        .attr('vector-effect', 'non-scaling-stroke')
         .style("fill", "steelblue");
 
 
