aff77119559f1db05461223741d7d837feb5db3b
Alex Myers <alex.myers@wustl.edu>
2022-02-16 11:49:08 -0600

    added year radio buttons

--- js/alex-main.js
+++ js/alex-main.js
@@ -12,7 +12,15 @@ const frame = d3.select("#POK_timemap")
 const vis = frame
      .append("g");
 
-let year = 1997;
+let day = "6";
+
+const getCompareString = function(num) {
+    if (num < 10)
+        return '2014-01-0' + num;
+    else
+        return '2014-01-' + num;
+}
+// TODO: make function that takes a number (6) and turns it into a string to compare Dates to (2014-01-06)
 
 
 d3.json("../data/all_data.json").then(function (data) {
@@ -28,6 +36,16 @@ d3.json("../data/all_data.json").then(function (data) {
     //         });
     //     info.text(this.value + "- " + data.info[year])
     // });
+    d3.selectAll("input[name='day']").on("change", function(){
+        day = this.value;
+        d3.selectAll('circle')
+                .attr("visibility", function (d) {
+                    if (d.Date === getCompareString(day))
+                        return "visible"
+                    else
+                        return "hidden"
+                });
+    });
 
     // the circles
     const circles = vis
@@ -39,18 +57,18 @@ d3.json("../data/all_data.json").then(function (data) {
         .style("fill",  "#9b5de8")
         .style("stroke", "black")
         .style("stroke-width", 1)
-        // .attr("visibility", function (d) {
-        //     if (d.startYear <= year)
-        //         return "visible"
-        //     else
-        //         return "hidden"
-        // });
+        .attr("visibility", function (d) {
+            if (d.Date === getCompareString(6))
+                return "visible"
+            else
+                return "hidden"
+        })
         .attr("cy", function(d) {
             return (((max_lat - d.Lat)/(max_lat - min_lat)) * h);
         })
         .attr("cx", function(d) {
             return (((d.Long - min_long)/(max_long - min_long)) * w);
-        })
+        });
 
 })
 
