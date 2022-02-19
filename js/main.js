0ffc1ebaa210cd8b863c306bd0f041113ed0a208
Saugat Pandey <p.saugat@wustl.edu>
2022-02-19 03:02:39 -0600

    fixing time filter

--- js/main.js
+++ js/main.js
@@ -15,22 +15,15 @@ const svg = d3.select(".POK_timemap")
 
 
 d3.json('../data/all_data.json').then(function (data) {
+
     const allName = new Set(data.map(function (d) {
         return d.Name
     }))
-    const allDate = new Set(data.map(function (d) {
-        return d.Date
-    }))
-    console.log(allDate)
 
     d3.select('.selectButton')
         .selectAll('myOptions')
         .data(allName).enter().append('option').text(function (d) { return d; }).attr('value', function (d) { return d; })
 
-
-    d3.select('.selectDate')
-        .selectAll('myOptions')
-        .data(allDate).enter().append('option').text(function (d) { return d; }).attr('value', function (d) { return d; })
 
     //creating a tooltip
     var tooltip = d3.select('.POK_timemap')
@@ -50,7 +43,7 @@ d3.json('../data/all_data.json').then(function (data) {
 
     const mousemove = function (event, d) {
         tooltip
-            .html(`Time: ${d.Time}`)
+            .html(`Time: ${d.Name}`)
             .style("left", (event.x) / 2 + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
             .style("top", (event.y) / 2 + "px")
     }
@@ -67,6 +60,7 @@ d3.json('../data/all_data.json').then(function (data) {
     //Color Scale for different names
     const myColor = d3.scaleOrdinal().domain(allName).range(d3.schemeSet2)
 
+    d3.selectAll('circles').remove();
     const circles = svg
         .selectAll("circle")
         .data(data.filter(function (d) {
@@ -90,12 +84,26 @@ d3.json('../data/all_data.json').then(function (data) {
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave);
 
+
+    function updateDate(setName) {
+        d3.select('.selectDate')
+            .selectAll('myOptions')
+            .data(setName).enter().append('option').text(function (d) { return d; }).attr('value', function (d) { return d; })
+    }
+    function updateTime1(setName) {
+        d3.select('.selectTime1')
+            .selectAll('myOptions')
+            .data(setName).enter().append('option').text(function (d) { return d; }).attr('value', function (d) { return d; })
+    }
+    function updateTime2(setName) {
+        d3.select('.selectTime2')
+            .selectAll('myOptions')
+            .data(setName).enter().append('option').text(function (d) { return d; }).attr('value', function (d) { return d; })
+    }
+
     //function to update the group
-    function update(selectOption, selectOption2) {
-        const filter_data = data.filter(function (d) {
-            return d.Name == selectOption && d.Date == selectOption2
-        })
-        circles.data(filter_data).transition().duration(800)
+    function update(typeData, selectOption) {
+        var dots = circles.data(typeData)
             .attr("cy", function (d) {
                 return (((max_lat - d.Lat) / (max_lat - min_lat)) * h);
             })
@@ -105,16 +113,54 @@ d3.json('../data/all_data.json').then(function (data) {
             .style('fill', function (d) {
                 return myColor(selectOption)
             })
-            .on("mouseover", mouseover)
+
+        dots.transition()
+            .duration(1000)
+            .delay(function (d, i) { return i * (1000 / 4); })
+            .style('opacity', 1);
+
+        dots.on("mouseover", mouseover)
             .on("mousemove", mousemove)
             .on("mouseleave", mouseleave);
     }
     d3.select('.selectButton').on('change', function (event, d) {
         const selectedGroup = d3.select(this).property('value')
         // update(selectedGroup);
+        const filtering = data.filter(function (d) {
+            return d.Name == selectedGroup
+        })
+        const allDate = new Set(filtering.map(function (d) {
+            return d.Date
+        }))
+        updateDate(allDate);
+
         d3.select('.selectDate').on('change', function (event, d) {
             const selectedDate = d3.select(this).property('value')
-            update(selectedGroup, selectedDate)
+            const filter_2 = data.filter(function (d) {
+                return d.Name == selectedGroup && d.Date == selectedDate
+            })
+            const allTime = new Set(filter_2.map(function (d) {
+                return d.Time
+            }))
+            updateTime1(allTime);
+            updateTime2(allTime);
+            //update(selectedGroup, selectedDate)
+            d3.select('.selectTime1').on('change', function (event, d) {
+                const selectedTime1 = d3.select(this).property('value')
+                const indexCalc = filter_2.map(d => d.Time).indexOf(selectedTime1)
+                console.log("The first index is", indexCalc)
+
+                d3.select('.selectTime2').on('change', function (event, d) {
+                    const selectedTime2 = d3.select(this).property('value')
+                    const indexCalc2 = filter_2.map(d => d.Time).indexOf(selectedTime2)
+                    console.log("The second index is", indexCalc2)
+                    const new_Data = filter_2.slice(indexCalc, indexCalc2 + 1)
+                    console.log(new_Data)
+                    update(new_Data, selectedGroup)
+                    //getRangeTime(selectedGroup, selectedDate, selectedTime1, selectedTime2)
+                    //console.log(getRangeTime(selectedGroup, selectedDate, selectedTime1, selectedTime2))
+                })
+            })
         })
     })
 })
